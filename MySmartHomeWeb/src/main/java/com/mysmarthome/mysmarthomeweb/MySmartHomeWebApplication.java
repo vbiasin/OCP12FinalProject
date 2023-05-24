244abdf8699d4589f8856423b0d9bc721f9c1eaa
vince <vincent.biasin@outlook.fr>
2023-05-24 19:42:38 +0200

    Add module web

---
+++ MySmartHomeWeb/src/main/java/com/mysmarthome/mysmarthomeweb/MySmartHomeWebApplication.java
@@ -0,0 +1,13 @@
+package com.mysmarthome.mysmarthomeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySmartHomeWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySmartHomeWebApplication.class, args);
+    }
+
+}
---
+++ MySmartHomeWeb/src/test/java/com/mysmarthome/mysmarthomeweb/MySmartHomeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysmarthome.mysmarthomeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySmartHomeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

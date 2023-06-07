7336dc1a0cc132b69bccc65ea5037422a67a3c03
vince <vincent.biasin@outlook.fr>
2023-06-07 18:43:32 +0200

    Add module spring cloud config

---
+++ MySmartHomeSpringCloudConfig/src/main/java/com/mysmarthome/mysmarthomespringcloudconfig/MySmartHomeSpringCloudConfigApplication.java
@@ -0,0 +1,13 @@
+package com.mysmarthome.mysmarthomespringcloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySmartHomeSpringCloudConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySmartHomeSpringCloudConfigApplication.class, args);
+    }
+
+}
---
+++ MySmartHomeSpringCloudConfig/src/test/java/com/mysmarthome/mysmarthomespringcloudconfig/MySmartHomeSpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysmarthome.mysmarthomespringcloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySmartHomeSpringCloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

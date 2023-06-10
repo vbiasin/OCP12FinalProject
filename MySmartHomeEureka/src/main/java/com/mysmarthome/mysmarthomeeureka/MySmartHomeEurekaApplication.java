a5f93f2ff2e147f2ca5382853f22ad1a8cf6637e
vince <vincent.biasin@outlook.fr>
2023-06-10 11:19:42 +0200

    ajout module Eureka

---
+++ MySmartHomeEureka/src/main/java/com/mysmarthome/mysmarthomeeureka/MySmartHomeEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.mysmarthome.mysmarthomeeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySmartHomeEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySmartHomeEurekaApplication.class, args);
+    }
+
+}
---
+++ MySmartHomeEureka/src/test/java/com/mysmarthome/mysmarthomeeureka/MySmartHomeEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysmarthome.mysmarthomeeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySmartHomeEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

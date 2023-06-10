125c927f19dae5ee3972ebe64f2d2e1841b6f563
vince <vincent.biasin@outlook.fr>
2023-06-10 11:28:40 +0200

    modifs pom

--- MySmartHomeSpringCloudConfig/src/main/java/com/mysmarthome/mysmarthomespringcloudconfig/MySmartHomeSpringCloudConfigApplication.java
+++ MySmartHomeSpringCloudConfig/src/main/java/com/mysmarthome/mysmarthomespringcloudconfig/MySmartHomeSpringCloudConfigApplication.java
@@ -2,8 +2,12 @@ package com.mysmarthome.mysmarthomespringcloudconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@EnableConfigServer
 public class MySmartHomeSpringCloudConfigApplication {
 
     public static void main(String[] args) {
